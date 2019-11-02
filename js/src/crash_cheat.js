58476c0e09ea68c418d2267249f56a6e907747cc
xiaoguii <xiaoguii.love@outlook.com>
2019-11-03 02:36:14 +0800

    Site updated: 2019-11-03 02:36:13

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,17 +1,16 @@
 <!-- 崩溃欺骗 -->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '泥憋走吖 (つ´ω`)つ';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ﾉ>ω<)ﾉ 欢迎回来 | ' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+var titleTime;
+var OriginTitle;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        OriginTitle = document.title; 
+        document.title = '泥憋走吖 (つ´ω`)つ';
+        clearTimeout(titleTime);
+    }
+    else {
+        document.title = '(ﾉ>ω<)ﾉ 欢迎回来 | ' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
